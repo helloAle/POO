ac3727c52dcef2887e662cab72f2e8523ebe7ec0
Alexandre Oliveira <89224363+helloAle@users.noreply.github.com>
2022-08-29 11:26:54 -0300

    Add files via upload

---
+++ financeiro/contas_a_pagar.java
@@ -0,0 +1,19 @@
+package financeiro;
+
+public class contas_a_pagar {
+	String pagar_conta;
+
+	public contas_a_pagar(String pagar_conta) {
+		super();
+		this.pagar_conta = pagar_conta;
+	}
+
+	public String getPagar_conta() {
+		return pagar_conta;
+	}
+
+	public void setPagar_conta(String pagar_conta) {
+		this.pagar_conta = pagar_conta;
+	}
+
+}
---
+++ financeiro/contas_a_receber.java
@@ -0,0 +1,19 @@
+package financeiro;
+
+public class contas_a_receber {
+	String receber_conta;
+
+	public contas_a_receber(String receber_conta) {
+		super();
+		this.receber_conta = receber_conta;
+	}
+
+	public String getReceber_conta() {
+		return receber_conta;
+	}
+
+	public void setReceber_conta(String receber_conta) {
+		this.receber_conta = receber_conta;
+	}
+
+}
