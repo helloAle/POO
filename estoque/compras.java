e7cf047fedba7d42b873898d9886fd98a42e73c4
Alexandre Oliveira <89224363+helloAle@users.noreply.github.com>
2022-08-29 11:26:02 -0300

    Add files via upload

---
+++ estoque/compras.java
@@ -0,0 +1,19 @@
+package estoque;
+
+public class compras {
+	String comprar;
+
+	public compras(String comprar) {
+		super();
+		this.comprar = comprar;
+	}
+
+	public String getComprar() {
+		return comprar;
+	}
+
+	public void setComprar(String comprar) {
+		this.comprar = comprar;
+	}
+
+}
---
+++ estoque/retirada.java
@@ -0,0 +1,19 @@
+package estoque;
+
+public class retirada {
+	String retirar;
+
+	public retirada(String retirar) {
+		super();
+		this.retirar = retirar;
+	}
+
+	public String getRetirar() {
+		return retirar;
+	}
+
+	public void setRetirar(String retirar) {
+		this.retirar = retirar;
+	}
+
+}
