d0277d737fbe70d2e10dfa126349fab45233baa0
Alexandre Oliveira <89224363+helloAle@users.noreply.github.com>
2022-08-29 11:24:12 -0300

    Add files via upload

---
+++ agenda/agendas.java
@@ -0,0 +1,11 @@
+package agenda;
+
+public class agendas {
+	String agenda;
+
+	public agendas(String agenda) {
+		super();
+		this.agenda = agenda;
+	}
+
+}
---
+++ agenda/contato.java
@@ -0,0 +1,11 @@
+package agenda;
+
+public class contato {
+	String contato;
+
+	public contato(String contato) {
+		super();
+		this.contato = contato;
+	}
+
+}
